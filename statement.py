afc5ac0a2e9ae6b6bce217262a5f95afc9c476b3
Dilpreet Kaur <dilpreetkaur@Dilpreets-Air.lan>
2018-01-29 00:47:27 +0800

    if else statements

---
+++ statement.py
@@ -0,0 +1,14 @@
+#! user/bin/env/python
+
+a = 20
+b = 8
+if a<b:
+    print("a is less than b")
+elif a>b+10:
+    print("a is greater than b by 10")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("a is smaller than b")
+
+
---
+++ test.py
@@ -0,0 +1,15 @@
+#! user/bin/env/python
+
+Name = "Pulsar 'abc'"
+p = 2.13
+P = p/1000
+
+LC = 4.77e4 * P
+print ("LC: %d km" %LC)
+
+if LC < 100:
+    print("%s has small radius of LC." % Name)
+else:
+    print("%s has normal radius of LC." % Name)
+
+
