48f1607c5247a9f4c3536e869a6309059755177d
Dilpreet Kaur <preet70291@gmail.com>
2018-03-22 11:52:29 +0800

    deleting files using unlink in loop

---
+++ mlist.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+
+directory = '/Users/dilpreetkaur/Documents/python/Data'
+for filename in os.listdir(directory):
+    if filename.endswith(".py"):
+        test= os.path.join(directory, filename)       
+	subprocess.call(["unlink", test])
+
+
+
+
