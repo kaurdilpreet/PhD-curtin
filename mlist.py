0088671de74162c523104fbdff26d76e0234570d
kaurdilpreet <35475235+kaurdilpreet@users.noreply.github.com>
2018-03-22 12:04:16 +0800

    Update mlist.py

--- mlist.py
+++ mlist.py
@@ -6,9 +6,10 @@ import subprocess
 
 directory = '/home/dilpreet/python/Data'
 for filename in os.listdir(directory):
-    if filename.endswith(".png"):
+    if filename.endswith(".py"):
         print(os.path.join(directory, filename))
-        subprocess.Popen(["unlink", "directory + filename"])
+        test = os.path.join(directory, filename)
+        subprocess.call(["unlink", test])
 
 
 
